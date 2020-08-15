c8f7b33b54a7bf2c6cac178ff422ea2bdbbc1127
Jeremy Richards <jeremyrichards2289@gmail.com>
2020-08-15 11:20:49 +0200

    task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,18 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Tasks.all
+  end
+
+  def details
+  end
+
+  def add
+  end
+
+  def edit
+  end
+
+  def remove
+  end
+end
+
